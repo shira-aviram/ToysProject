4f47946677f0c247d6207ee613f581bd89f334f6
shira-aviram <s214124307@gmail.com>
2023-12-04 10:24:00 +0200

    update3

--- api/routes/toys.js
+++ api/routes/toys.js
@@ -49,18 +49,47 @@ router.get("/category/:catName", async(req,res)=>{
     res.status(500).json({msg:"there error try again later",err})
   }    
 })
-// router.get("/prices/", async(req,res)=>{
-//   try{
-//     let paramC = req.params.catName;
-//     let data = await ToyModel.find({category:paramC})
-//     .limit(50)
-//     res.json(data);
-//   }
-//   catch(err){
-//     console.log(err);
-//     res.status(500).json({msg:"there error try again later",err})
-//   }    
-// })
+router.get("/prices", async (req, res) => {
+  let perPage = req.query.perPage || 10;
+  let page = req.query.page || 1;
+  let sort = req.query.sort || "price"
+  let reverse = req.query.reverse == "yes" ? -1 : 1;
+  try {
+      let minP = req.query.min;
+      let maxP = req.query.max;
+      if (minP && maxP) {
+          let data = await ToyModel.find({ $and: [{ price: { $gte: minP } }, { price: { $lte: maxP } }] })
+
+              .limit(perPage)
+              .skip((page - 1) * perPage)
+              .sort({ [sort]: reverse })
+          res.json(data);
+      }
+      else if (maxP) {
+          let data = await ToyModel.find({ price: { $lte: maxP } })
+              .limit(perPage)
+              .skip((page - 1) * perPage)
+              .sort({ [sort]: reverse })
+          res.json(data);
+      } else if (minP) {
+          let data = await ToyModel.find({ price: { $gte: minP } })
+              .limit(perPage)
+              .skip((page - 1) * perPage)
+              .sort({ [sort]: reverse })
+          res.json(data);
+      } else {
+          let data = await ToyModel.find({})
+              .limit(perPage)
+              .skip((page - 1) * perPage)
+              .sort({ [sort]: reverse })
+          res.json(data);
+      }
+  }
+  catch (err) {
+      console.log(err);
+      res.status(500).json({ msg: "there error try again later", err })
+  }
+})
 
 router.post("/", auth,async(req,res) => {
   let validBody = validateToys(req.body);
